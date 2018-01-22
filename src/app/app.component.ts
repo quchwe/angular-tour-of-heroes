d62a07f2ebf990c6dd1c4d31499f2cd0dc27dbfe
quchaowei <quchaowei@ysten.com>
2018-01-22 20:58:49 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+  <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {HeroService} from './hero-detail/hero.service';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{
+      path: 'heroes',
+      component: HeroesComponent
+    }, {
+      path: 'dashboard',
+      component: DashboardComponent
+    }, {
+      path: '',
+      redirectTo: '/dashboard',
+      pathMatch: 'full'
+    }, {
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    }, ])
+
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
